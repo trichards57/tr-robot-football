650d106b6419983757c37270ffe66f06994ec047
Trichards57@gmail.com <Trichards57@gmail.com@2a34e269-b2c9-bc40-89ed-5a513ac304bc>
2012-02-07 23:40:47 +0000

    Started implementing A* algorithm

---
+++ trunk/Robot Football/RobotFootball/AStarRouteFinder/GridSquare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AStarRouteFinder
+{
+    class GridSquare
+    {
+        private bool obstacle = false;
+
+        public int? Weight { get; set; }
+        public SquareType Type { get; set; }
+
+    }
+
+    public enum SquareType
+    {
+        Origin,
+        Obstacle,
+        Destination
+    }
+}

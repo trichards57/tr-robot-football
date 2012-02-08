d1a84ba5b6b54a8c6403654e47b43d4baad3f8f3
Trichards57@gmail.com <Trichards57@gmail.com@2a34e269-b2c9-bc40-89ed-5a513ac304bc>
2012-02-08 23:28:18 +0000

    Fixing previous commit

---
+++ trunk/Robot Football/RobotFootball/RobotFootballCore/Utilities/PointExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace RobotFootballCore.Utilities
+{
+    public static class PointExtensions
+    {
+        public static PointF Scale(this PointF point, SizeF resolution)
+        {
+            var x = point.X / resolution.Width;
+            var y = point.Y / resolution.Height;
+
+            return new PointF(x, y);
+        }
+
+        public static PointF Scale(this Point point, SizeF resolution)
+        {
+            var x = (float)point.X / resolution.Width;
+            var y = (float)point.Y / resolution.Height;
+
+            return new PointF(x, y);
+        }
+
+        public static Point FromIndex(int index, int width)
+        {
+            return new Point(index % width, index / width);
+        }
+
+        public static int ToIndex(this Point point, int width)
+        {
+            return point.X + point.Y * width;
+        }
+
+        public static Point Floor(this PointF point)
+        {
+            return new Point((int)Math.Floor(point.X), (int)Math.Floor(point.Y));
+        }
+    }
+}
---
+++ trunk/Robot Football/RobotFootball/RobotFootballCore/Utilities/SizeExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace RobotFootballCore.Utilities
+{
+    public static class SizeExtensions
+    {
+        public static SizeF Invert(this SizeF size)
+        {
+            if (size.Width == 0 || size.Height == 0)
+                throw new ArgumentOutOfRangeException("size", size, "size must not have zero width or height");
+            var width = 1.0f / size.Width;
+            var height = 1.0f / size.Height;
+
+            return new SizeF(width, height);
+        }
+
+        public static SizeF Invert(this Size size)
+        {
+            if (size.Width == 0 || size.Height == 0)
+                throw new ArgumentOutOfRangeException("size", size, "size must not have zero width or height");
+            var width = 1.0f / size.Width;
+            var height = 1.0f / size.Height;
+
+            return new SizeF(width, height);
+        }
+
+        public static SizeF Scale(this SizeF size, SizeF scale)
+        {
+            var width = size.Width / scale.Width;
+            var height = size.Height / scale.Height;
+
+            return new SizeF(width, height);
+        }
+
+        public static SizeF Scale(this Size size, float scale)
+        {
+            var width = (float)size.Width / scale;
+            var height = (float)size.Height / scale;
+
+            return new SizeF(width, height);
+        }
+
+        public static SizeF Scale(this Size size, SizeF scale)
+        {
+            var width = (float)size.Width / scale.Width;
+            var height = (float)size.Height / scale.Height;
+
+            return new SizeF(width, height);
+        }
+
+        public static SizeF Scale(this SizeF size, float scale)
+        {
+            var width = size.Width / scale;
+            var height = size.Height / scale;
+
+            return new SizeF(width, height);
+        }
+
+        public static Size Ceiling(this SizeF size)
+        {
+            var width = (int)Math.Ceiling(size.Width);
+            var height = (int)Math.Ceiling(size.Height);
+
+            return new Size(width, height);
+        }
+    }
+}

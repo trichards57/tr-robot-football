d47e5f8d2165e36a4211abdca3887f7ae5d1a845
Trichards57@gmail.com <Trichards57@gmail.com@2a34e269-b2c9-bc40-89ed-5a513ac304bc>
2012-02-08 23:28:18 +0000

    Fixing previous commit

---
+++ Robot Football/RobotFootball/RobotFootballCore/Interfaces/RouteFinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RobotFootballCore.RouteObjects;
+using System.Drawing;
+using RobotFootballCore.Objects;
+
+namespace RobotFootballCore.Interfaces
+{
+    public abstract class RouteFinder
+    {
+        public abstract Route FindPath(PointF startPoint, PointF endPoint, Field field, IPositionedObject movingObject);
+
+        public Size Resolution { get; set; }
+    }
+}

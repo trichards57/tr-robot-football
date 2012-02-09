042633c1b771cb338d3ea870a3ad1f70f39325b3
Trichards57@gmail.com <Trichards57@gmail.com@2a34e269-b2c9-bc40-89ed-5a513ac304bc>
2012-02-09 23:10:21 +0000

    Continuing to fix previous commit.

---
+++ Robot Football/RobotFootball/AStarRouteFinder/PotentialFieldRouteFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RobotFootballCore.Interfaces;
+using RobotFootballCore.RouteObjects;
+using System.Drawing;
+using RobotFootballCore.Objects;
+using RobotFootballCore.Utilities;
+using System.Threading.Tasks;
+
+namespace RouteFinders
+{
+    public class PotentialFieldRouteFinder : RouteFinder
+    {
+        public override Route FindPath(PointF startPoint, PointF endPoint, Field field, IPositionedObject movingObject)
+        {
+            const int attractiveConstant = 1/10;
+            const int repulsiveConstant = 10;
+            const int repulsiveDistance = 20;
+
+            var currentVelocity = Size.Empty;
+            var currentPosition = startPoint;
+
+            var route = new Route();
+
+            while (CalculateLength(currentPosition, endPoint) > 10)
+            {
+                var distance = new SizeF(currentPosition.X - endPoint.X, currentPosition.Y - endPoint.Y);
+                var attractForce = distance.Scale(attractiveConstant);
+
+                var repulseForce = 0;
+
+                Parallel.ForEach(field.Players.Where(p => p.Team == Team.Opposition), p =>
+                {
+
+                });
+            }
+
+            return route;
+        }
+    }
+}

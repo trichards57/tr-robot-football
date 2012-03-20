10056c4c1c926857d2e4bf661fac70136a76a736
Trichards57@gmail.com <Trichards57@gmail.com@2a34e269-b2c9-bc40-89ed-5a513ac304bc>
2012-03-20 11:27:04 +0000

    Documented PathController.

--- Robot Football/RobotFootball/SquareStrategy/PathController.cpp
+++ Robot Football/RobotFootball/SquareStrategy/PathController.cpp
@@ -2,12 +2,24 @@
 
 #include "PathController.h"
 
+/// Registers a path for the given robot.  Will replace any path already registered to that robot.
+///
+/// @param path The path to register
+/// @param bot The robot to assign the path to
 void PathController::RegisterPath(Path path, int bot)
 {
 	currentPath[bot] = path;
 	currentTimeOffset[bot] = 0;
 }
 
+/// Steps through all the registered paths, controlling each robot to follow the provided Path.  If the
+/// control function returns a target point of (-1,-1), the controller will move to the next ControlFunction
+/// in the path.  If the end of the path is reached, it will instruct the robot's motors to stop (with no
+/// velocity control, just 0 on each motor.
+///
+/// @param env The environment containing the robots to move
+/// @param controller The MotionController to use to move the robot
+/// @param velocities The velocity feedback required by the MotionController
 bool PathController::StepPaths(Environment* env, MotionController* controller, Vector3D* velocities)
 {
 	auto currentTime = clock();
@@ -41,13 +53,4 @@ bool PathController::StepPaths(Environment* env, MotionController* controller, V
 		}
 	}
 	return true;
-}
-
-PathController::PathController(void)
-{
-}
-
-
-PathController::~PathController(void)
-{
 }
--- Robot Football/RobotFootball/SquareStrategy/PathController.h
+++ Robot Football/RobotFootball/SquareStrategy/PathController.h
@@ -8,19 +8,27 @@
 #include "..\..\..\Simulator\Strategy Source\Strategy.h"
 #include "MotionController.h"
 
+/// @brief A function that produces a Vector3D based on the input time
 typedef std::function<Vector3D(clock_t)> ControlFunction;
 
+/// @brief A list of ControlFunction that make up a path.
 typedef std::vector<ControlFunction> Path;
 
+/// @brief A controller that will make the robots follow a set of pre-registered paths
+///
+/// Holds a collection of Path for each robot, and issues the required commands to move
+/// the robots along that path on every StepPaths.
 class PathController
 {
 private:
+	/// @brief The current path for each robot
 	std::map<int, Path> currentPath;
+	/// @brief The current time offset used to run each path
 	std::map<int, clock_t> currentTimeOffset;
 public:
-	PathController(void);
-	~PathController(void);
+	/// @brief Registers a path for the given robot
 	void RegisterPath(Path path, int bot);
+	/// @brief Steps each registered path, using the provided MotionController
 	bool StepPaths(Environment* env, MotionController* controller, Vector3D* velocities);
 };
 

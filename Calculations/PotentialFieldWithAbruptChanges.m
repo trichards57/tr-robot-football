9ee14b37a495f3ad2a4406eb4a7d2cdf23e68042
Trichards57@gmail.com <Trichards57@gmail.com@2a34e269-b2c9-bc40-89ed-5a513ac304bc>
2012-03-22 14:06:29 +0000

    Rest of the potential field check in

--- Calculations/PotentialFieldWithAbruptChanges.m
+++ Calculations/PotentialFieldWithAbruptChanges.m
@@ -2,8 +2,8 @@ clear all
 close all
 
 resolution = 0.5;
-fieldWidth = 220;
-fieldHeight = 180;
+fieldWidth = 87;
+fieldHeight = 71;
 
 attractField = zeros(fieldWidth/resolution,fieldHeight/resolution);
 repelField = zeros(fieldWidth/resolution,fieldHeight/resolution);
@@ -11,29 +11,29 @@ speedX = zeros(fieldWidth/resolution,fieldHeight/resolution);
 speedY = zeros(fieldWidth/resolution,fieldHeight/resolution);
 
 ball = BasicObject;
-ball.X = 110;
-ball.Y = 90;
+ball.X = fieldWidth/2;
+ball.Y = fieldHeight/2;
 ball.Weight = 150;
 
 opponentsWeight = 1;
-opponentsEffectRadius = 100;
+opponentsEffectRadius = 10;
 
 opponents(5) = BasicObject;
 
-opponents(1).X = 55;
-opponents(1).Y = 50;
+opponents(1).X = 22;
+opponents(1).Y = 20;
 opponents(1).Weight = opponentsWeight;
-opponents(2).X = (fieldWidth-55);
-opponents(2).Y = 50;
+opponents(2).X = (fieldWidth-22);
+opponents(2).Y = 20;
 opponents(2).Weight = opponentsWeight;
-opponents(3).X = (fieldWidth-55);
-opponents(3).Y = (fieldHeight-50);
+opponents(3).X = (fieldWidth-22);
+opponents(3).Y = (fieldHeight-20);
 opponents(3).Weight = opponentsWeight;
-opponents(4).X = 55;
-opponents(4).Y = (fieldHeight-50);
+opponents(4).X = 22;
+opponents(4).Y = (fieldHeight-20);
 opponents(4).Weight = opponentsWeight;
 
-opponents(5).X = (fieldWidth-15);
+opponents(5).X = (fieldWidth-6);
 opponents(5).Y = fieldHeight/2;
 opponents(5).Weight = opponentsWeight;
 
@@ -50,14 +50,14 @@ for x= 1:resolution:fieldWidth
         for k=1:size(opponents,2)
             xDiff = opponents(k).X - x;
             yDiff = opponents(k).Y - y;
-            dist = sqrt(xDiff^2 + yDiff^2) - 11; % 11 cm to allow for width of the robot
+            dist = sqrt(xDiff^2 + yDiff^2) - 5; % 5" to allow for width of the robot
             
             dist = max(dist, 0.001);
             
-            if (dist < opponentsEffectRadius)
+            %if (dist < opponentsEffectRadius)
                 we = (1/2 * opponents(k).Weight * (1/dist - 1/opponentsEffectRadius)^2);
                 repelField(i,j) = repelField(i,j) + we;
-            end
+            %end
         end
     end
 end
@@ -76,11 +76,14 @@ for x= 1:resolution:fieldWidth
         
         angle = atan(py(i,j)/px(i,j));
         
-        if (px(i,j) == abs(px(i,j)))
+        if (px(i,j) ~= abs(px(i,j)))
             angle = angle + pi;
         end
         
         angleToTarget = atan(yDiff/xDiff);
+        if (xDiff ~= abs(xDiff))
+            angleToTarget = angleToTarget + pi;
+        end
         motionAngle = angle-angleToTarget;
         
         if repelField(i,j)==0
@@ -88,17 +91,14 @@ for x= 1:resolution:fieldWidth
             speedY(i,j) = 100*sin(angle);
         else
             speedX(i,j) = 100*(1-abs(cos(motionAngle)))*cos(angle);
-            speedY(i,j) = 100*sin(angle);
+            speedY(i,j) = 100*(1-abs(cos(motionAngle)))*sin(angle);
         end
     end
 end
 
-x = 1:resolution:fieldWidth;
-y = 1:resolution:fieldHeight;
-
-surf(x, y, field', 'EdgeColor', 'None');
+surf(field', 'EdgeColor', 'None');
 figure;
 [px,py] = gradient(field');
-contour(x, y, field'), hold on, quiver(x,y,-px,-py), hold off
+contour(field'), hold on, quiver(-px,-py), hold off
 figure;
-quiver(x,y, speedY', speedX');
+quiver(speedY', speedX');
